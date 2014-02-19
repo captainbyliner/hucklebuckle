ec012adc8532f48f917dfab3b81d3f57ddd29e0e
captainbyliner <mcarthurdonald@gmail.com>
2014-02-18 23:46:18 -0500

    Final changes to my shopping app! Added trash cans and lengthened container to 1000px for larger browsers. Knock on wood, we are golden!

--- application.js
+++ application.js
@@ -13,7 +13,16 @@ $(document).ready(function(){
 
     function addItem() {
     var itemText = $.trim( $("#listItem").val() );
+
+    var img = $("<img />" , {
+        src:
+        "http://meditateinkansas.org/wp-content/uploads/2013/03/psd-trash-can-icon-copy-e1362863883898.png" ,
+        class: "deleteItem"
+    });
+
     var item = $("<li />", { text: itemText, title: "Double click to remove from list."});
+
+    item.append(img);
     $("#list").append(item);
     $("#listItem").val('');
 }
@@ -23,12 +32,19 @@ $(document).ready(function(){
 });
 
 
-$("ul#list").on('click', 'li', function() {
+
+ $("#list").on('click', '.deleteItem', function() {
+        $(this).parent().remove();
+    });
+
+
+
+$("#list").on('click', 'li', function() {
         $(this).toggleClass('strike');
     });
 
 $("ul#list").on('dblclick' , 'li', function(){
-        $(this).hide();
+        $(this).remove();
 });
 
 
