832994aa00229cc5530a6e47a8d68613b85ba50f
captainbyliner <mcarthurdonald@gmail.com>
2014-02-18 21:50:48 -0500

    updated jquery and some css. Oh, so close. Need to set min-height so panel gets bigger but can't quite figure it out.

--- application.js
+++ application.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 
     function addItem() {
     var itemText = $("#listItem").val();
-    var item = $("<li />", { text: itemText });
+    var item = $("<li />", { text: itemText, title: "Double click to remove from list."});
     $("#list").append(item);
     $("#listItem").val('');
 }
@@ -21,15 +21,21 @@ $(document).ready(function(){
 
 });
 
+
 $("ul#list").on('click', 'li', function() {
         $(this).toggleClass('strike');
     });
+
+$("ul#list").on('dblclick' , 'li', function(){
+        $(this).hide();
+});
+
 
 $("#listItem").click(function() {
     $(".panelText").fadeOut("slow");
     $("#removeItem").show("slow");
     $("#saleItem").show("slow");
-
+    $("#listItem").css("background-color" , "yellow");
 });
 
 
